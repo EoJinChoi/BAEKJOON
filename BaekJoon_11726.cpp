7ed76d00d70fb90790f3f9e15f3db1c4c7fd38e8
EoJinChoi <01031763910a@gmail.com>
2024-05-01 15:43:59 +0900

    first commit

---
+++ BaekJoon_11726.cpp
@@ -0,0 +1,26 @@
+// 2024.5.1
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    vector<int> v;
+
+    cin >> n;
+
+    v.push_back(1);
+    v.push_back(2);
+
+    for (int i = 2; i < n; i++)
+    {
+        v.push_back((v.at(i - 1) + v.at(i - 2)) % 10007);
+    }
+
+    cout << v.at(n - 1) << endl;
+
+    return 0;
+}
